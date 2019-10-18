71d7721cbb2664642b5841d99db152aceeb0647d
Myles Dolan <myles.dolan@benefacts.ie>
2019-10-18 17:53:51 +0100

    m

--- routes/api/twit.js
+++ routes/api/twit.js
@@ -12,7 +12,47 @@ const Twit = require('twit');
 
 
 router.get('/getname', (req, res) => {
+
+    console.log("fuckfucko");
+
+
+
+
+
+    const requestBody = { user_id: '706939025521823700',
+        'authorization': 'OAuth',
+        'oauth_consumer_key':'v2DfHU49qZnzgoGSMBQdTj0Qn',
+        'oauth_nonce':'generated_oauth_nonce',
+        'oauth_signature':'NlGcOP2CizAp6ccHNHBdzU6hkmW4nLjTuJDYmZCLQd9hL',
+        'oauth_signature_method':'HMAC-SHA1',
+        'oauth_timestamp':'generated_timestamp',
+        'oauth_token':'482460052-hcPdD6OnxNLjS2LqnbAXXpAQ4qKF8JZv3xDBvX89',
+        'oauth_version':"1.0"
+
+
+
+    };
+
+    const config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+
+
+        }
+    };
+    const qs = require('querystring');
+
+  //  axios.post('https://api.twitter.com/1.1//friendships/destroy.json', qs.stringify(requestBody), config)
+    axios.post('https://api.twitter.com/1.1//friendships/destroy.json')
+
+        .then((result) => { console.log("Destroy result",result)
+
+        })
+        .catch((err) => {console.log("err",err)})
+    ;
     console.log("fuckfuck");
+
+
 
    // console.log ("im so here",req.params.code);
     let T = new Twit({
@@ -32,15 +72,18 @@ router.get('/getname', (req, res) => {
     T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
         console.log(data);
         res.json(data);
-    })
+    });
 
-  //  T.post("friendships/destroy/:user_id", { user_id: '706939025521823700' }, function (err, data, response) {
+
+
+  /*  T.post("friendships/destroy/:user_id", { user_id: '706939025521823700' }, function (err, data, response) {
     T.post("friendships/destroy/:id", { id: '706939025521823700' }, function (err, data, response) {
 
         console.log("destroyDATAx",data);
         console.log("destroyResponsex",response);
         console.log("destroyErrx",err);
     })
+*/
 
         // T.get('followers/ids', { screen_name: 'tolga_tezel' },  function (err, data, response) {
     T.get('followers/ids', { screen_name: 'Myles Dolan' },  function (err, data, response) {
@@ -56,12 +99,11 @@ router.get('/getname', (req, res) => {
             console.log("idddd",allids[i]);
                // T.post('statuses/destroy/:id', { id: allids[i] }, function (err, data, response) {
               //  T.post('friendships/destroy/:user_id', { user_id: allids[i] }, function (err, data, response) {
-                T.post('friendships/destroy/:user_id', { user_id: allids[i] }, function (err, data, response) {
-
-                        console.log("destroyDATA",data);
-                    console.log("destroyResponse",response);
-                    console.log("destroyErr",err);
-                })
+              //  T.post('friendships/destroy/:user_id', { user_id: allids[i] }, function (err, data, response) {
+                //        console.log("destroyDATA",data);
+                  //  console.log("destroyResponse",response);
+                   //console.log("destroyErr",err);
+               // })
 
 
             }
