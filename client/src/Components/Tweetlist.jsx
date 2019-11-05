6bbc81251f4809cc79f3e502b906628d88c9d7b6
Myles Dolan <myles.dolan@benefacts.ie>
2019-11-05 17:31:50 +0000

    at bladdy last

--- client/src/Components/Tweetlist.jsx
+++ client/src/Components/Tweetlist.jsx
@@ -3,20 +3,34 @@ import { Link } from 'react-router-dom';
 
 const Tweeterlist = (props) =>{
     console.log("insidetweeterlist", props.tweets.statuses);
-    return (
-        <div className="tweet_container">
-            if (props.tweets.statuses )
-            {
-                props.tweets.statuses && props.tweets.statuses.length > 0 ?
-                        props.tweets.statuses.map(item => (
 
-                            <div>
-                                something xxxxxxxxxxx
-                                {item.id_str}
-                            </div>
-                        )) : null
-                }
-}
+  //  console.log("insidetweeterlist", props.tweets.statuses.length);
+    console.log("insidetweeterlist2",props.tweets.length);
+    //props.tweets.statuses && props.tweets.statuses.length > 0 ?
+    //const arr = [...props.tweets(100)].map((_, i) => i);
+
+    return (
+        <div >
+ho
+            {
+                props.tweets.statuses  ?
+
+
+
+                   //props.tweets.map((item,index) => (
+
+                    //  props.tweets['statuses'].map(item => (
+                    props.tweets.statuses.statuses.map((item,index) => (
+
+                        <div>
+                            something xxxxxxxxxxx
+                            {item.id_str}
+                        </div>
+                    )) : null
+
+            }
+
+
         </div>
     )
 }
